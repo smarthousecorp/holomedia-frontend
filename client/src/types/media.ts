f264e6972eec41161d87ea12045bf9937804c727
YujunSun0 <yujunsun0@gmail.com>
2024-10-31 16:13:27 +0900

    Feat : 영상 데이터 타입 작성

---
+++ client/src/types/media.ts
@@ -0,0 +1,10 @@
+// 영상 단일 데이터 타입
+export interface media {
+  id: number;
+  title: string;
+  url: string;
+  views: number;
+  non_thumbnail: string;
+  member_thumbnail: string;
+  name: string;
+}
