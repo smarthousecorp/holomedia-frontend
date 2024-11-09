7aac06ced64823441a12afd6977d59cd507fb33d
YujunSun <yujunsun0@gmail.com>
2024-11-09 16:42:57 +0900

    Fix : firebase 업로드 중복 문제 해결

--- client/src/hooks/useUploadImage.ts
+++ client/src/hooks/useUploadImage.ts
@@ -1,13 +1,12 @@
-import {getDownloadURL, ref, uploadBytes} from "firebase/storage";
-import {fstorage} from "../firebase";
-import {v4 as uuidv4} from "uuid";
+import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
+import { fstorage } from "../firebase";
+import { v4 as uuidv4 } from "uuid";
 
 export default function useUploadImage() {
-  const storageRef = ref(fstorage, `HOLOMEDIA/${uuidv4()}`);
-  // firebase storage에 files이란 폴더가 만들어지고 그 안에 생성된 uuid 이름으로 이미지파일이 저장되게끔 한다.
-
   const uploadImage = async (file: File) => {
-    const snapshot = await uploadBytes(storageRef, file);
+    // 각 업로드마다 새로운 uuid 생성
+    const imageRef = ref(fstorage, `HOLOMEDIA/${uuidv4()}`);
+    const snapshot = await uploadBytes(imageRef, file);
     const downloadUrl = await getDownloadURL(snapshot.ref);
     return downloadUrl;
   };
